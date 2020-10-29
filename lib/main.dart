1f7b0ead0225d8874421d0922ade76b813ba356e
Bismark de Carvalho <47608746+Bismark-Carvalho@users.noreply.github.com>
2020-10-28 21:00:00 -0300

    iniciando repo

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:buscador_gif/screen/HomePage.dart';
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
